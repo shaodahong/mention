f0abe316e7445c12cdf7b894135f21a48c043a88
shao <darryshaw@gmail.com>
2019-10-24 17:13:19 +0800

    add mention click

--- src/index.tsx
+++ src/index.tsx
@@ -33,21 +33,21 @@ const propertyNamesToCopy = [
 function Mention() {
   const [top, setTop] = useState(0)
   const [left, setLeft] = useState(0)
+  const [value, setValue] = useState('')
   const textareaRef = useRef<HTMLTextAreaElement>()
 
   const onKeyUp: React.KeyboardEventHandler<HTMLTextAreaElement> = event => {
     const { key } = event
+
     if (key === '@') {
+      const { current } = textareaRef
+      const parrent = current.parentElement
+      const end = current.selectionEnd
       const child = document.getElementById('mirror')
 
       if (child && child.parentElement) {
         child.parentElement.removeChild(child)
       }
-
-      const { current } = textareaRef
-      const parrent = current.parentElement
-      const end = current.selectionEnd
-      // current.setSelectionRange(1, 1)
 
       const beforeText = current.value.slice(0, end)
       const afterText = current.value.slice(end)
@@ -59,7 +59,7 @@ function Mention() {
       const style = window.getComputedStyle(current)
       const props = []
       props.push(
-        'white-space:pre-wrap;position:absolute;top:-2px;left:0;z-index:1;overflow:auto;word-wrap: break-word;',
+        'white-space:pre-wrap;position:absolute;top:-2px;left:0;z-index:-1;overflow:auto;word-wrap: break-word;',
       )
 
       for (let i = 0, len = propertyNamesToCopy.length; i < len; i++) {
@@ -91,13 +91,47 @@ function Mention() {
     }
   }
 
+  const onInput: React.FormEventHandler<HTMLTextAreaElement> = event => {
+    const { value } = event.target as any
+    setValue(value)
+  }
+
+  function replaceByPosition(
+    text: string,
+    start: number,
+    stop: number,
+    replacetext: string,
+  ) {
+    return text.substring(0, start) + replacetext + text.substring(stop)
+  }
+
+  const onMentionClick = (mention: string) => {
+    const { current } = textareaRef
+    const end = current.selectionEnd
+    const flagIndex = value.lastIndexOf('@', end)
+
+    if (flagIndex < 0) {
+      return
+    }
+
+    const insertString = `@${mention} `
+    const rangPosition = end + insertString.length - 1
+    setValue(replaceByPosition(value, flagIndex, end, insertString))
+    current.focus()
+    setTimeout(() => {
+      current.setSelectionRange(rangPosition, rangPosition)
+    }, 100)
+  }
+
   return (
     <div className="text-area-wrap">
       <textarea
         className="text-area"
         ref={textareaRef}
-        onKeyUp={onKeyUp}
         placeholder="placeholder"
+        value={value}
+        onKeyUp={onKeyUp}
+        onInput={onInput}
       ></textarea>
       <ul
         className="mentions"
@@ -106,9 +140,11 @@ function Mention() {
           top,
         }}
       >
-        <li>123</li>
-        <li>456</li>
-        <li>789</li>
+        {['123', '456', '789'].map(v => (
+          <li key={v} onClick={onMentionClick.bind(this, v)}>
+            {v}
+          </li>
+        ))}
       </ul>
     </div>
   )
